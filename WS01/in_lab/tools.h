4b2ba40afee66aa93abe3d6023fc559b8629da44
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-01-17 13:44:10 -0500

    W1 inlab

---
+++ WS01/in_lab/tools.h
@@ -0,0 +1,12 @@
+// Workshop 1 in-lab
+// Name: ADAM STINZIANI
+// Student #: 124521188
+// Course: OOP244 Winter 2019
+// File: tools.h
+
+// Displays the user interface menu
+int menu();
+
+// Performs a fool-proof integer entry
+int getInt(int min, int max);
+
