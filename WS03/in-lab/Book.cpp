26b7d91d8962f10f194ba68e0438ac27ba7f86cd
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-01-30 13:31:37 -0500

    add

--- WS03/in-lab/Book.cpp
+++ WS03/in-lab/Book.cpp
@@ -2,4 +2,8 @@
 // Name:		ADAM STINZIANI
 // Student #:	124521188
 // Course:		OOP244 Winter 2019
-// File:		Book.h
+// File:		Book.cpp
+
+#include <iostream>
+#include <cstring>
+#include "Book.h"
--- WS03/in-lab/Book.h
+++ WS03/in-lab/Book.h
@@ -7,7 +7,7 @@
 #ifndef SICT_BOOK_H
 #define SICT_BOOK_H
 
-#define MAX_TITLE_SIZE 32
+#define MAX_TITLE_SIZE 32	//PUT IN NAMESPACE???
 #define MAX_NAME_SIZE 16
 #define MIN_ISBN_VALUE 1000000000000
 #define MAX_ISBN_VALUE 9999999999999
@@ -16,6 +16,11 @@ namespace sict {
 	class Book {
 		char lastName[MAX_NAME_SIZE];
 		char firstName[MAX_NAME_SIZE];
+		double ISBN;
+	public:
+		void set(char firstName, char lastName, char title, int ISBN);
+		bool isEmpty() const;
+		void display() const;
 	};
 }
 
