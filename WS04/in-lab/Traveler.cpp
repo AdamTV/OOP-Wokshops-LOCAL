e114c01fb86e893e666f387a1d5928c6142a4a01
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-02-06 12:40:54 -0500

    OMG

---
+++ WS04/in-lab/Traveler.cpp
@@ -0,0 +1,32 @@
+// TODO: add file header comments here
+// Workshop:	4
+// Name:		ADAM STINZIANI
+// Student #:	124521188
+// Course:		OOP244 Winter 2019
+// File:		Traveler.cpp
+
+// TODO: add your headers here
+#include <iostream>
+#include "Traveler.h"
+
+// TODO: continue your namespace here
+namespace sict {
+
+   // TODO: implement the default constructor here
+	Traveler::Traveler() {
+		firstName[0] = '\0';	//SAFE EMPTY STATE
+	}
+    // TODO: implement the constructor with 3 parameters here
+	Traveler::Traveler(char* frst, char* last, char* dest) {
+		if (this->firstName[0] != '\0') {
+
+		}
+	}
+	Traveler::~Traveler() {
+
+	}
+    // TODO: implement isEmpty query here
+
+    // TODO: implement display query here
+}
+
---
+++ WS04/in-lab/Traveler.h
@@ -0,0 +1,29 @@
+// TODO: add file header comments here
+// Workshop:	4
+// Name:		ADAM STINZIANI
+// Student #:	124521188
+// Course:		OOP244 Winter 2019
+// File:		Traveler.h
+
+// TODO: add header file guard here
+#ifndef SICT_TRAVELER_H
+#define SICT_TRAVELER_H
+
+// TODO: declare your namespace here
+namespace sict {
+
+	// TODO: define the constants here
+	const int max_destination_size = 32;
+	const int max_name_size = 16;
+
+		// TODO: define the Traveler class here
+	class Traveler {
+		char firstName[max_name_size + 1];
+		char lastName[max_name_size + 1];
+		char destination[max_destination_size + 1];
+		Traveler();
+		Traveler(char*, char*, char*);
+		~Traveler();
+	};
+}
+#endif // !SICT_TRAVELER_H
