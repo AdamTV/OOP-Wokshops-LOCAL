1ac0be2f8217bc45f47f475408c08115ce4b428f
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-02-12 21:37:42 -0500

    lab

---
+++ WS05/in-lab/Fraction.h
@@ -0,0 +1,35 @@
+// Workshop:	5
+// Name:		ADAM STINZIANI
+// Student #:	124521188
+// Course:		OOP244 Winter 2019
+// File:		Fraction.h
+
+// TODO: header file guard
+#ifndef SICT_FRACTION_H
+#define SICT_FRACTION_H
+
+
+// TODO: create namespace
+namespace sict {
+	// TODO: define the Fraction class
+	class Fraction {
+		// TODO: declare instance variables 
+		int a;
+		int b;
+		// TODO: declare private member functions
+		int max() const;
+		int min() const;
+		void reduce();
+		int gcd() const;
+	public:
+		// TODO: declare public member functions
+		void display() const;
+		bool isEmpty() const;
+		Fraction();
+		Fraction(int, int);
+		// TODO: declare the + operator overload
+		Fraction operator+(const Fraction& rhs) const;
+	};
+}
+#endif // !0SICT_FRACTION_H
+
